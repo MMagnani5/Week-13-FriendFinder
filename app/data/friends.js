755aa4c2d788b075d382d95c9f6f3443b2c04f7a
Mike Magnani <mikemagnani5@gmail.com>
2016-07-31 18:12:17 -0400

    add to js files

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+// Friends Array that will populate with the users response
+var friendsArray =
+  [
+
+    {
+        "name":"Keanu",
+        "photo":"http://images.complex.com/complex/image/upload/t_article_image/keanu-the-cat_dn8vba.jpg",
+        "scores":[
+          1,
+          5,
+          5,
+          1,
+          5,
+          1,
+          4,
+          2,
+          5,
+          5
+      ]
+    }
+
+  ];
+  module.exports = (friendsArray);
